264472f0c657dd64c7d315454d125564c8a9288c
Mihhail Kornyshev <mihhail@codeborne.com>
2023-02-15 12:18:17 +0200

    Remove obsolete annotations

--- src/main/java/com/codeborne/json/JsonParser.kt
+++ src/main/java/com/codeborne/json/JsonParser.kt
@@ -1,7 +1,6 @@
 package com.codeborne.json
 
 import org.intellij.lang.annotations.Language
-import java.io.IOException
 import java.io.Reader
 import java.io.StringReader
 
@@ -9,7 +8,6 @@ import java.io.StringReader
  * [JSON specification](https://www.json.org/json-en.html)
  */
 class JsonParser {
-    @Throws(IOException::class, JsonParseException::class)
     fun parse(@Language("JSON") input: String?): Any? {
         return parse(StringReader(input))
     }
