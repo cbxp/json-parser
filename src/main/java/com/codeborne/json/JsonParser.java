7918824ccc699ffb59c0aedf90023a63865d1ade
Valeri Kuzmin <valeri@codeborne.com>
2023-02-15 15:25:29 +0200

    double with e

--- src/main/java/com/codeborne/json/JsonParser.java
+++ src/main/java/com/codeborne/json/JsonParser.java
@@ -43,6 +43,7 @@ public class JsonParser {
         case "\f":
           break;
         case ".":
+        case "e":
           isDouble = true;
         default:
           buffer = buffer + stringValue;
