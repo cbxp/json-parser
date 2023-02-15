4c80580c3ebd3d65773fcd865cadab97d26e0f7b
Erik Puidet <erikpuidet@gmail.com>
2023-02-15 13:33:36 +0200

    Format according to IuteCodeStyle.xml

--- src/main/java/com/codeborne/json/JsonParser.java
+++ src/main/java/com/codeborne/json/JsonParser.java
@@ -12,6 +12,7 @@ import org.intellij.lang.annotations.Language;
  * <a href="https://www.json.org/json-en.html">JSON specification</a>
  */
 public class JsonParser {
+
   public Object parse(@Language("JSON") String input) throws IOException, JsonParseException {
     return parse(new StringReader(input));
   }
@@ -116,4 +117,5 @@ public class JsonParser {
       }
     }
   }
+
 }
