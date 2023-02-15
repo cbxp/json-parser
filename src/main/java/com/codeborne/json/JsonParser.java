2b11a3d435c7d59e23bc3b405f46ad389a503575
Valeri Kuzmin <valeri@codeborne.com>
2023-02-15 14:42:32 +0200

    read string with dedicated method

--- src/main/java/com/codeborne/json/JsonParser.java
+++ src/main/java/com/codeborne/json/JsonParser.java
@@ -16,7 +16,7 @@ import java.util.Map;
  */
 public class JsonParser {
   @Nullable
-  private static Object readValue(Reader input, Boolean isString) throws IOException {
+  private static Object readValue(Reader input) throws IOException {
     int value;
     String buffer = "";
     boolean isDouble = false;
@@ -32,19 +32,12 @@ public class JsonParser {
         case "}":
         case "]":
         case ",":
-          if (!isString) {
-            exit = true;
-            break;
-          }
-        case "\\":
-          //TODO other escapes
-          buffer = buffer + String.valueOf(Character.toChars(input.read()));
+          exit = true;
           break;
         case "\"":
-          if (isString) return buffer;
-          else return readValue(input, true);
+          return readString(input);
         case " ":
-          if (!isString) break;
+          break;
         case ".":
           isDouble = true;
         default:
@@ -54,9 +47,6 @@ public class JsonParser {
     }
     switch (buffer) {
       case "":
-        if (isString) {
-          return "";
-        }
         return null;
       case "null":
         return null;
@@ -65,7 +55,6 @@ public class JsonParser {
       case "false":
         return false;
       default:
-        if (isString) return buffer;
         if (isDouble) {
           return Double.valueOf(buffer);
         } else {
@@ -79,6 +68,24 @@ public class JsonParser {
     }
   }
 
+  private static String readString(Reader input) throws IOException {
+    int value;
+    StringBuilder buffer = new StringBuilder();
+    while ((value = input.read()) != -1) {
+      String stringValue = String.valueOf(Character.toChars(value));
+      switch (stringValue) {
+        case "\\":
+          buffer.append(String.valueOf(Character.toChars(input.read())));
+          break;
+        case "\"":
+          return buffer.toString();
+        default:
+          buffer.append(stringValue);
+      }
+    }
+    return buffer.toString();
+  }
+
   private static Map<String, Object> readObject(Reader input) throws IOException {
     Map<String, Object> o = new HashMap<>();
     Map<String, Object> v;
@@ -89,9 +96,9 @@ public class JsonParser {
   }
 
   private static Map<String, Object> readKeyValue(Reader input) throws IOException {
-    var key = (String)readValue(input, false);
-    readValue(input, false);
-    var value = readValue(input, false);
+    var key = (String) readValue(input);
+    readValue(input);
+    var value = readValue(input);
     if (key == null) return null;
     return Map.of(key, value);
   }
@@ -99,7 +106,7 @@ public class JsonParser {
   private static List<Object> readArray(Reader input) throws IOException {
     List<Object> objects = new ArrayList<>();
     Object v = null;
-    while ((v = readValue(input, false)) != null) {
+    while ((v = readValue(input)) != null) {
       objects.add(v);
     }
     return objects;
@@ -110,7 +117,7 @@ public class JsonParser {
   }
 
   public Object parse(Reader input) throws IOException, JsonParseException {
-    return readValue(input, false);
+    return readValue(input);
   }
 }
 
