6ca1076112ea2e9cc671027df91649309d3f833c
Silver Somma <silvertiit@gmail.com>
2023-02-15 14:19:56 +0200

    parse simple objects

--- src/main/java/com/codeborne/json/JsonParser.java
+++ src/main/java/com/codeborne/json/JsonParser.java
@@ -1,24 +1,101 @@
 package com.codeborne.json;
 
 import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * <a href="https://www.json.org/json-en.html">JSON specification</a>
  */
 public class JsonParser {
-  public Object parse(@Language("JSON") String input) throws IOException, JsonParseException {
-    return parse(new StringReader(input));
-  }
+    public Object parse(@Language("JSON") String input) throws IOException, JsonParseException {
+        return parse(new StringReader(input));
+    }
 
-  public Object parse(Reader input) throws IOException, JsonParseException {
-    // TODO implement me
-    return null;
-  }
+    public Object parse(Reader input) throws IOException, JsonParseException {
+        // TODO implement me
+        StringBuilder contentBuilder = new StringBuilder();
+        input.mark(Integer.MAX_VALUE);
+        int i = input.read();
+        if (i == 91) {
+            return parseArray(input, contentBuilder);
+        } else if (i == 123) {
+            return parseObject(input, contentBuilder);
+        }
+        input.reset();
+        return parseItem(input);
+    }
+
+    private Map<Object, Object> parseObject(Reader input, StringBuilder contentBuilder) throws IOException, JsonParseException {
+        int i;
+        i = input.read();
+        while (i != 125) {
+            contentBuilder.append((char) i);
+            i = input.read();
+        }
+        Map<Object, Object> map = new HashMap<>();
+        String content = contentBuilder.toString();
+        String[] entries = content.split(",");
+        for (String entry : entries) {
+            String[] entryItems = entry.split(":");
+            map.put(parseItem(new StringReader(entryItems[0].trim())), parseItem(new StringReader(entryItems[1].trim())));
+        }
+        return map;
+    }
+
+    private List<Object> parseArray(Reader input, StringBuilder contentBuilder) throws IOException {
+        int i;
+        i = input.read();
+        while (i != 93) {
+            contentBuilder.append((char) i);
+            i = input.read();
+        }
+        String content = contentBuilder.toString();
+        String[] arrayItems = content.split(",");
+        return Arrays.stream(arrayItems).map(item -> parseItem(new StringReader(item.trim()))).collect(Collectors.toList());
+    }
+
+    private static Object parseItem(Reader input) {
+        StringBuilder content = new StringBuilder();
+        int i = 0;
+        try {
+            i = input.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        while (i != -1) {
+            if (i != 34) {
+                content.append((char) i);
+            }
+            try {
+                i = input.read();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if ("null".equals(content.toString())) return null;
+        if ("true".equals(content.toString())) return true;
+        if ("false".equals(content.toString())) return false;
+        try {
+            return Integer.valueOf(content.toString());
+        } catch (NumberFormatException ignored) {
+        }
+        try {
+            return Double.valueOf(content.toString());
+        } catch (NumberFormatException ignored) {
+        }
+        return content.toString();
+    }
 }
 
 /*
--- src/test/java/com/codeborne/json/JsonParserTest.java
+++ src/test/java/com/codeborne/json/JsonParserTest.java
@@ -1,14 +1,55 @@
 package com.codeborne.json;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static com.codeborne.json.assertions.JsonAssertions.assertThat;
 
 class JsonParserTest {
-  private final JsonParser parser = new JsonParser();
+    private final JsonParser parser = new JsonParser();
 
-  @Test
-  void plainNull() throws Exception {
-    assertThat(parser.parse("null")).isNull();
-  }
+    @Test
+    void plainNull() throws Exception {
+        assertThat(parser.parse("null")).isNull();
+    }
+
+    @Test
+    void jsonWithString() throws IOException, JsonParseException {
+        assertThat(parser.parse("\"string\"")).isEqualTo("string");
+    }
+
+    @Test
+    void jsonWithNumber() throws IOException, JsonParseException {
+        assertThat(parser.parse("9")).isEqualTo(9);
+    }
+    @Test
+    void jsonWithDecimalNumber() throws IOException, JsonParseException {
+        assertThat(parser.parse("9.9")).isEqualTo(9.9);
+    }
+    @Test
+    void jsonWithArray() throws IOException, JsonParseException {
+        assertThat(parser.parse("[\"string\",\"string2\"]")).isEqualTo(List.of("string","string2"));
+    }
+    @Test
+    void jsonWithOneFieldObject() throws IOException, JsonParseException {
+        assertThat(parser.parse("{\"string\": \"stringValue\"}")).isEqualTo(Map.of("string", "stringValue"));
+    }
+
+    @Test
+    void jsonWithMultipleFieldObject() throws IOException, JsonParseException {
+        assertThat(parser.parse("{\"string\": \"stringValue\", \"number\" : 10.10}")).isEqualTo(Map.of("string", "stringValue", "number", 10.10));
+    }
+    @Test
+    void jsonWithObjectInsideOfAnObject() throws IOException, JsonParseException {
+        assertThat(parser.parse("{\"string\":{\"stringKey\": \"stringValue\"}}")).isEqualTo(Map.of("string", Map.of("stringKey", "stringValue")));
+    }
+    @Test
+    @Disabled
+    void jsonWithArrayOfObjects() throws IOException, JsonParseException {
+        assertThat(parser.parse("[{\"string\": \"stringValue\"},{\"string\": \"stringValue2\"}]")).isEqualTo(List.of(Map.of("string", "stringValue"),Map.of("string", "stringValue2")));
+    }
 }
